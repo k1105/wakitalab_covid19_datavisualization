bbdb128d6b1f6952eef2eabd0fbf63c2abb4e722
k1105 <kanapon1105@yahoo.co.jp>
2022-11-17 12:24:14 +0900

    stateのリフトアップ

--- components/MeshGroup.tsx
+++ components/MeshGroup.tsx
@@ -29,53 +29,43 @@ type PrefPopulation = { population: number[] };
 type Props = {
   focusedPrefId: number;
   setDate: Dispatch<SetStateAction<string>>;
+  covidCases: Case[];
+  prefPopulation: PrefPopulation;
+  prefLatLon: PrefLatLon[];
+  setCaseCount: Dispatch<SetStateAction<number>>;
 };
 
 extend({ OrbitControls });
 
-export default function MeshGroup({ focusedPrefId, setDate }: Props) {
-  const covidCases = useRef<Case[]>();
-  const prefLatLon = useRef<PrefLatLon[]>();
-  const prefPopulation = useRef<PrefPopulation>();
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+export default function MeshGroup({
+  focusedPrefId,
+  setDate,
+  covidCases,
+  prefPopulation,
+  prefLatLon,
+  setCaseCount,
+}: Props) {
   const elapsedTime = useRef<number>(0);
   const groupRef = useRef<Group>(null);
 
-  useMemo(() => {
-    const getJson = async () => {
-      prefLatLon.current = await fetch("/data/pref_lat_lon.json").then((data) =>
-        data.json()
-      );
-      covidCases.current = await fetch("/data/casesMin.json").then((data) =>
-        data.json()
-      );
-      prefPopulation.current = await fetch("/data/pref_population.json").then(
-        (data) => data.json()
-      );
-    };
-    getJson().then(() => {
-      setIsLoading(false);
-    });
-  }, []);
-
   const controls = useRef<OrbitControls>(null);
   const { camera, gl } = useThree();
-  const offset = 200;
+  const offset = 800;
   useFrame((state, delta) => {
     const current: number = Math.floor(elapsedTime.current);
     const next = current + 1;
     const t = elapsedTime.current - current;
     elapsedTime.current = elapsedTime.current + delta;
     controls.current?.update();
-    if (covidCases.current && prefPopulation.current) {
+    if (covidCases && prefPopulation) {
       for (let i = 0; i < 47; i++) {
         const currentSize: number =
-          ((covidCases.current as Case[])[current + offset].case[i] /
-            prefPopulation.current.population[i]) *
+          ((covidCases as Case[])[current + offset].case[i] /
+            prefPopulation.population[i]) *
           1000;
         const nextSize: number =
-          ((covidCases.current as Case[])[next + offset].case[i] /
-            prefPopulation.current.population[i]) *
+          ((covidCases as Case[])[next + offset].case[i] /
+            prefPopulation.population[i]) *
           1000;
         const size = t * nextSize + (1 - t) * currentSize;
         groupRef.current?.children[i].children[1].scale.set(size, size, size);
@@ -85,85 +75,78 @@ export default function MeshGroup({ focusedPrefId, setDate }: Props) {
   });
 
   setInterval(() => {
-    typeof covidCases.current == "undefined"
-      ? setDate("")
-      : setDate(
-          covidCases.current[Math.floor(elapsedTime.current) + offset].date
-        );
-  }, 100);
+    setDate(covidCases[Math.floor(elapsedTime.current) + offset].date);
+    setCaseCount(
+      covidCases[Math.floor(elapsedTime.current) + offset].case[focusedPrefId]
+    );
+  }, 1000);
 
   return (
     <>
-      {isLoading ? (
-        <></>
-      ) : (
-        <group ref={groupRef}>
-          {(() => {
-            const meshes = [];
-            for (let i = 0; i < 47; i++) {
-              const size = 10;
-              const pref_name = (prefLatLon.current as PrefLatLon[])[i]
-                .pref_name;
-              const pref_lon = Number(
-                (prefLatLon.current as PrefLatLon[])[i].lon
+      <group ref={groupRef}>
+        {(() => {
+          const meshes = [];
+          for (let i = 0; i < 47; i++) {
+            const size = 10;
+            const pref_name = (prefLatLon as PrefLatLon[])[i].pref_name;
+            const pref_lon = Number((prefLatLon as PrefLatLon[])[i].lon);
+            const pref_lat = Number((prefLatLon as PrefLatLon[])[i].lat);
+            if (i == focusedPrefId) {
+              meshes.push(
+                <group>
+                  <Text
+                    fontSize={0.07}
+                    color={0x000000}
+                    position={[pref_lon - 135, 0, 35 - pref_lat]}
+                  >
+                    {pref_name}
+                  </Text>
+                  <mesh
+                    key={i}
+                    position={[pref_lon - 135, 0, 35 - pref_lat]}
+                    scale={[size, size, size]}
+                  >
+                    <sphereGeometry />
+                    <meshBasicMaterial
+                      color="#00ffff"
+                      wireframe={true}
+                      transparent={true}
+                      opacity={0.5}
+                    />
+                  </mesh>
+                </group>
               );
-              const pref_lat = Number(
-                (prefLatLon.current as PrefLatLon[])[i].lat
+            } else {
+              meshes.push(
+                <group>
+                  <Text
+                    fontSize={0.07}
+                    color={0x000000}
+                    position={[pref_lon - 135, 0, 35 - pref_lat]}
+                  >
+                    {pref_name}
+                  </Text>
+                  <mesh
+                    key={i}
+                    position={[pref_lon - 135, 0, 35 - pref_lat]}
+                    scale={[size, size, size]}
+                  >
+                    <sphereGeometry />
+                    <meshBasicMaterial
+                      color="#cccccc"
+                      wireframe={true}
+                      transparent={true}
+                      opacity={0.5}
+                    />
+                  </mesh>
+                </group>
               );
-              if (i == focusedPrefId) {
-                meshes.push(
-                  <group>
-                    <Text
-                      color={0x000000}
-                      position={[pref_lon - 135, 0, 35 - pref_lat]}
-                    >
-                      {pref_name}
-                    </Text>
-                    <mesh
-                      key={i}
-                      position={[pref_lon - 135, 0, 35 - pref_lat]}
-                      scale={[size, size, size]}
-                    >
-                      <sphereGeometry />
-                      <meshBasicMaterial
-                        color="#00ffff"
-                        wireframe={true}
-                        transparent={true}
-                        opacity={0.5}
-                      />
-                    </mesh>
-                  </group>
-                );
-              } else {
-                meshes.push(
-                  <group>
-                    <Text
-                      color={0x000000}
-                      position={[pref_lon - 135, 0, 35 - pref_lat]}
-                    >
-                      {pref_name}
-                    </Text>
-                    <mesh
-                      key={i}
-                      position={[pref_lon - 135, 0, 35 - pref_lat]}
-                      scale={[size, size, size]}
-                    >
-                      <sphereGeometry />
-                      <meshBasicMaterial
-                        color="#cccccc"
-                        wireframe={true}
-                        transparent={true}
-                        opacity={0.5}
-                      />
-                    </mesh>
-                  </group>
-                );
-              }
             }
-            return meshes;
-          })()}
-        </group>
-      )}
+          }
+          return meshes;
+        })()}
+      </group>
+
       <orbitControls
         ref={controls}
         args={[camera, gl.domElement]}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,72 @@
 import { Canvas } from "@react-three/fiber";
 import { extend } from "@react-three/fiber";
-import { useState } from "react";
+import { useState, useRef, useMemo } from "react";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 import MeshGroup from "../components/MeshGroup";
 
 extend({ OrbitControls });
 
+type Case = { case: number[]; all: number; date: string };
+type PrefLatLon = { pref_name: string; lat: string; lon: string };
+type PrefPopulation = { population: number[] };
+
 export default function Home() {
   const [date, setDate] = useState<string>("");
   const [focusedPrefId, setFocusedPrefId] = useState<number>(15);
+  const covidCases = useRef<Case[]>();
+  const prefLatLon = useRef<PrefLatLon[]>();
+  const prefPopulation = useRef<PrefPopulation>();
+  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [caseCount, setCaseCount] = useState<number>(0);
+
+  useMemo(() => {
+    const getJson = async () => {
+      prefLatLon.current = await fetch("/data/pref_lat_lon.json").then((data) =>
+        data.json()
+      );
+      covidCases.current = await fetch("/data/casesMin.json").then((data) =>
+        data.json()
+      );
+      prefPopulation.current = await fetch("/data/pref_population.json").then(
+        (data) => data.json()
+      );
+    };
+    getJson().then(() => {
+      setIsLoading(false);
+    });
+  }, []);
   return (
     <div id="root">
       <button onClick={() => setFocusedPrefId((focusedPrefId + 1) % 47)}>
         Next
       </button>
-      <div style={{ position: "absolute", fontSize: "2rem" }}>{date}</div>
+      <button onClick={() => setFocusedPrefId((focusedPrefId - 1 + 47) % 47)}>
+        Prev
+      </button>
+      <div style={{ position: "absolute", fontSize: "1.5rem" }}>
+        <p>
+          <strong>
+            {typeof prefLatLon.current == "undefined"
+              ? ""
+              : prefLatLon.current[focusedPrefId].pref_name}
+          </strong>
+        </p>
+        <p>{date}</p>
+        <p>{caseCount}</p>
+      </div>
       <Canvas>
-        <MeshGroup focusedPrefId={focusedPrefId} setDate={setDate} />
+        {isLoading ? (
+          <></>
+        ) : (
+          <MeshGroup
+            focusedPrefId={focusedPrefId}
+            setDate={setDate}
+            prefLatLon={prefLatLon.current as PrefLatLon[]}
+            prefPopulation={prefPopulation.current as PrefPopulation}
+            covidCases={covidCases.current as Case[]}
+            setCaseCount={setCaseCount}
+          />
+        )}
       </Canvas>
     </div>
   );
