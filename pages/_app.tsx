89ba6fc353821cb520bf86f660e6ab7387e5e2ae
k1105 <kanapon1105@yahoo.co.jp>
2022-12-27 16:34:13 +0900

    title要素の追加

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,14 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main>
+      <Head>
+        <title>at that time — COVID-19 Pandemic</title>
+      </Head>
+      <Component {...pageProps} />
+    </main>
+  );
 }
